0493faa3a169e73952b115bf870a740837129d8c
Alan Thai <alan.thai@shopify.com>
2020-03-05 15:13:35 -0500

    [react-form] reduceFields utilities

--- packages/react-form/src/test/utilities.test.ts
+++ packages/react-form/src/test/utilities.test.ts
@@ -1,4 +1,32 @@
-import {shallowArrayComparison, isChangeEvent} from '../utilities';
+import {
+  shallowArrayComparison,
+  isChangeEvent,
+  getValues,
+  noop,
+  validateAll,
+  fieldsToArray,
+} from '../utilities';
+import {Field} from '../types';
+
+function mockField<T>(params: Partial<Field<T>> | T): Field<T> {
+  const value = typeof params === 'string' ? params : undefined;
+  const fieldProps = typeof params === 'string' ? {} : params;
+
+  return {
+    value,
+    error: undefined,
+    defaultValue: undefined,
+    touched: false,
+    dirty: false,
+    onBlur: noop,
+    onChange: noop,
+    runValidation: () => '',
+    setError: noop,
+    newDefaultValue: noop,
+    reset: noop,
+    ...fieldProps,
+  };
+}
 
 describe('shallowArrayComparison()', () => {
   describe('when the two arrays are the same', () => {
@@ -20,8 +48,82 @@ describe('shallowArrayComparison()', () => {
   });
 });
 
-describe('isChangeEvent', () => {
+describe('isChangeEvent()', () => {
   it('returns false on null', () => {
     expect(isChangeEvent(null)).toBe(false);
+  });
+});
+
+describe('getValues()', () => {
+  it('returns form value', () => {
+    const formValue = {
+      username: 'myUsername',
+      name: {
+        firstName: 'myFirstName',
+      },
+      emails: [
+        {
+          work: 'work@example.com',
+        },
+      ],
+    };
+    const fieldBag = {
+      username: mockField(formValue.username),
+      name: {
+        firstName: mockField(formValue.name.firstName),
+      },
+      emails: [
+        {
+          work: mockField(formValue.emails[0].work),
+        },
+      ],
+    };
+
+    expect(getValues(fieldBag)).toEqual(formValue);
+  });
+});
+
+describe('validateAll', () => {
+  it('returns array of field errors', () => {
+    function identity<T>(v: T) {
+      return () => v;
+    }
+    const fieldWithError = (message: string) =>
+      mockField({runValidation: identity(message)});
+
+    const fieldBag = {
+      username: fieldWithError('Invalid username'),
+      name: {
+        firstName: fieldWithError('Invalid first name'),
+      },
+      emails: [
+        {
+          work: fieldWithError('Invalid work email'),
+        },
+      ],
+    };
+
+    const errors = validateAll(fieldBag);
+    expect(errors).toContainEqual({message: 'Invalid username'});
+    expect(errors).toContainEqual({message: 'Invalid first name'});
+    expect(errors).toContainEqual({message: 'Invalid work email'});
+  });
+});
+
+describe('fieldsToArray()', () => {
+  it('returns flat list of fields', () => {
+    const username = mockField('');
+    const firstName = mockField('');
+    const work = mockField('');
+    const fieldBag = {
+      username,
+      name: {firstName},
+      emails: [{work}],
+    };
+
+    const fieldsArray = fieldsToArray(fieldBag);
+    expect(fieldsArray).toContain(username);
+    expect(fieldsArray).toContain(firstName);
+    expect(fieldsArray).toContain(work);
   });
 });
